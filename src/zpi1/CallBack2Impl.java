bf89736b29a860b5dd0fb8ae6588d90e7d073a58
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-10-18 10:41:39 +0200

    Dodanie pustej implementacji CallBack2

---
+++ src/zpi1/CallBack2Impl.java
@@ -0,0 +1,12 @@
+package zpi1;
+
+/**
+ * Created by mawieczo on 10/18/16.
+ */
+public class CallBack2Impl implements CallBack2 {
+
+    @Override
+    public boolean getInformationAboutResult() {
+        return false;
+    }
+}
